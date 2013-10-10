cd16bfbb81310cbcc48410661e306e72751c22cf
Irsael viveros <israel.viveros@outlook.com>
2013-10-10 17:46:48 -0500

    updates scren_first

--- screen_first.php
+++ screen_first.php
@@ -13,6 +13,7 @@ include('functions.php');
 $mysqli = new mysqli(URL_DATABASE, USER_DATABASE, PASSWORD_DATABASE, TABLE_DATABASE);
 if ($mysqli->connect_errno) {
     echo "Fallo al contenctar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    $ErrorFlag = TRUE;
 }
 
 /*
@@ -20,15 +21,21 @@ if ($mysqli->connect_errno) {
  */
 if ($mysqli->connect_errno) {
     printf("Ha Fallado la conexion con la Base de datos: %s\n", $mysqli->connect_error);
+    $ErrorFlag = TRUE;
     exit();
 }
 
 
+if (isset($ErrorFlag)) {
+	echo "Se  nececita tener conexion con la base de datos";
+}else{
+	$available_dates = $mysqli->query("select fecha from fecha where DAY(fecha) >= '10' AND MONTH(fecha)= '10' AND YEAR(fecha) = '2013'");
+	 while ($row = mysqli_fetch_assoc($available_dates)){
+	  	echo $row['fecha']."<br>";
+	  }
+}
 
-$available_dates = $mysqli->query("select fecha from fecha where DAY(fecha) >= '10' AND MONTH(fecha)= '10' AND YEAR(fecha) = '2013'");
- while ($row = mysqli_fetch_assoc($available_dates)){
-  	echo $row['fecha']."<br>";
-  }
+
 
 
 
